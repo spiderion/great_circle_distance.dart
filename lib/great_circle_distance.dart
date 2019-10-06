78142d40f1a691227a1496c587b90eb615c68f14
ionolaru <spideiron@live.it>
2019-10-06 09:52:04 +0100

    change name

--- lib/great_circle_distance.dart
+++ lib/great_circle_distance.dart
@@ -2,6 +2,6 @@
 /// having a pair of Latitude/Longitude points
 /// The great-circle is shortest distance between two points on the surface of a sphere
 /// Check: https://en.wikipedia.org/wiki/Great-circle_distance
-library great_circle_distance;
+library great_circle_distance_ion;
 
 export 'src/great_circle_distance_base.dart';
